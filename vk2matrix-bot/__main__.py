43c3c7e04f5d9e2a57b783ef5bb618ca9ae193b6
saber-nyan <saber-nyan@ya.ru>
2018-02-04 20:45:31 +0300

    Simple ping-echo command is working!
    + Configuration via ENV variables
    + Exception handling

--- vk2matrix-bot/__main__.py
+++ vk2matrix-bot/__main__.py
@@ -2,16 +2,124 @@
 """
 Our main file!
 """
+import logging
+import signal
 import sys
+import time
+from typing import Dict
+
+from matrix_bot_api.matrix_bot_api import MatrixBotAPI
+from matrix_bot_api.mcommand_handler import MCommandHandler
+from matrix_client.room import Room
+
+try:
+    from . import config
+except ImportError:
+    import config
+
+EXIT_SUCCESS = 0  # By SIGINT/SIGTERM, successfully
+EXIT_ENV = 1  # Incorrect ENV variables
+EXIT_VK_API = 2  # vk_api error
+EXIT_MATRIX_API = 4  # matrix_bot_api error
+EXIT_UNKNOWN = 128  # WTF?!
+
+# event Dict:
+# {
+#   'origin_server_ts':10000000000,
+#   'sender':'@username:matrix.org',
+#   'event_id':'$ididid:matrix.org',
+#   'unsigned':{
+#     'age':602
+#   },
+#   'content':{
+#     'body':'!ping',
+#     'msgtype':'m.text'
+#   },
+#   'type':'m.room.message',
+#   'room_id':'!ololololo:matrix.org'
+# }
 
 
-def main():
-    pass
+def bot_cmd_ping_echo(room: Room, event: Dict[str, str]):
+    """
+    Check bot availability, echo text.
+
+    :param Room room: Matrix room
+    :param dict event: event content
+    """
+    # noinspection PyTypeChecker
+    room.send_html("ALIVE!<br/>"
+                   "<b>You sent:</b> {}".format(event["content"]["body"]))
+
+
+def register_bot_callbacks(bot: MatrixBotAPI):
+    """
+    Define all handlers and register callbacks.
+
+    :param MatrixBotAPI bot: bot instance
+    """
+    bot.add_handler(MCommandHandler('ping', bot_cmd_ping_echo))
+
+
+# noinspection PyBroadException
+def main() -> int:
+    """
+    Our main() function!
+
+    :return: exit code
+    :rtype: int
+    """
+    log.info("Init MatrixBotAPI...")
+    try:
+        bot = MatrixBotAPI(config.USERNAME, config.PASSWORD, config.SERVER)
+        register_bot_callbacks(bot)
+        bot.start_polling()
+        log.info("...success, started polling")
+    except:
+        log.critical("...failure!\nDetails:\n", exc_info=1)
+        return EXIT_MATRIX_API
+
+    log.info("Suspending MainThread")
+    while True:
+        try:
+            time.sleep(2.628e+6)  # Approximately 1 month...
+        except SystemExit:
+            return EXIT_SUCCESS
+        log.info("A month passed, lol~")
+
+
+# noinspection PyUnusedLocal
+def exit_handler(sig, frame):
+    """
+    SIGINT (^C)/SIGTERM handler for graceful shutdown.
+    """
+    log.info('Bye!')
+    sys.exit(EXIT_SUCCESS)
 
 
 if __name__ == '__main__':
     print("init...")
+
+    l_logger = logging.getLogger()
+    l_logger.setLevel(config.LOG_LEVEL)
+    l_logger_sh = logging.StreamHandler()
+    l_logger_sh.setFormatter(logging.Formatter(config.LOG_FORMAT))
+    l_logger_sh.setLevel(config.LOG_LEVEL)
+    l_logger.addHandler(l_logger_sh)
+
+    log = l_logger
+
+    signal.signal(signal.SIGINT, exit_handler)
+    signal.signal(signal.SIGTERM, exit_handler)
+
+    # noinspection PyBroadException
     try:
-        sys.exit(main())
+        status = main()
     except:
-        pass
+        log.critical("Unknown exception was caught!\n"
+                     "You can report issue w/ following information at:\n"
+                     "https://github.com/saber-nyan/vk2matrix-bot/issues\n"
+                     "(But do not report problems with API servers or network connection!)\n\n"
+                     "Details:\n", exc_info=1)
+        sys.exit(EXIT_UNKNOWN)
+    sys.exit(status)
---
+++ vk2matrix-bot/config.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Bot configuration file.
+It takes settings from environment variables.
+"""
+import os
+import sys
+import traceback
+
+# var = os.getenv('key', default)
+
+# IMPORTANT: each variable has a MATRIX_ prefix to avoid conflicts.
+
+p = 'MATRIX_'
+
+# noinspection PyBroadException
+try:
+    USERNAME = os.environ[p + 'USERNAME']  # Matrix bot username
+    PASSWORD = os.environ[p + 'PASSWORD']  # Matrix bot password
+except:
+    print("Please set necessary ENV variables!\n"
+          "Read more in config.py or README.md.\n\n"
+          "Details:\n{}".format(traceback.format_exc()), file=sys.stderr)
+    sys.exit(1)
+
+# Matrix server URL. Auth at https://vector.im?
+SERVER = os.getenv(p + 'SERVER', "https://matrix.org")
+
+
+logfmt_default = '%(asctime)s (%(filename)s:%(lineno)d %(threadName)s) %(levelname)s - %(name)s: %(message)s'
+LOG_FORMAT = os.getenv(p + 'LOG_FORMAT', logfmt_default)
+
+# Available levels:
+# CRITICAL
+# ERROR
+# WARNING
+# INFO
+# DEBUG
+# NOTSET
+LOG_LEVEL = os.getenv(p + 'LOG_LEVEL', 'INFO')
+
